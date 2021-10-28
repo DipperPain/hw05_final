aaf628b8044a3a67d53c7097dde4b8fa99416972
DipperPain <flexalexanimal@gmail.com>
2021-10-28 19:33:39 +0300

    ready

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
 ]
 
 
-#handler404 = 'core.views.page_not_found'
-#handler500 = 'core.views.server_error'
-#handler403 = 'core.views.permission_denied'
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
     urlpatterns += static(
